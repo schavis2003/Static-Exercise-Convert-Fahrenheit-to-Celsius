a257369ddbd4d49da2d770a1fdb436426ba42372
schavis2022 <schavis2022>
2022-06-15 12:58:37 -0400

    Converting Fahrenheit to Celsius

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            Console.WriteLine("What is the temperature in F you want to convert");
+            double fahTemp = Convert.ToDouble(Console.ReadLine());
+            double convertToCel = TempConverter.FahrenheitToCelsius(fahTemp);
+
+            Console.WriteLine($"That equals {convertToCel} degrees Celsius");
+
+            Console.WriteLine("What is the temperature in C you want to convert");
+            double CelTemp = Convert.ToDouble(Console.ReadLine());
+            double convertToFah = TempConverter.FahrenheitToCelsius(CelTemp);
+
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fTemp) 
+        {
+            var celTemp = (fTemp - 32) * (5 / 9);
+            return celTemp;
+        }
+
+        public static double CelsiusToFaharenheit(double cTemp)
+        {   
+            var celTemp = (cTemp - 32) * (5 / 9);
+            return (cTemp = 32) * (5 / 9); 
+        }
+
+        //Now create a static class called TempConverter. The class should have at least 2 methods, one called FahrenheitToCelsius that will require a double as a parameter and return a double,
+        //the other CelsiusToFahrenheit which will also require a double as a parameter and return a double.
+
+    }
+}
